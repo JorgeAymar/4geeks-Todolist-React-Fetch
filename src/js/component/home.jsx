b8d47b1b056074ebe94d5c2d29c25cfc50f02d2f
Jorge Sánchez Aymar <jorge.s.aymar@gmail.com>
2023-03-24 01:25:31 +0000

    last update

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
@@ -34,16 +32,37 @@ const Home = () => {
 	}, []);
 
 	return (
-		<div className="container">
-			<div className="row text-center">
+		<div>
+			<div className="container mt-5">
+				<div className="row justify-content-center">
+					<div className="col-lg-6 col-md-8 col-sm-10">
+						<h1 className="text-center mb-5">Todo List</h1>
 
-				<h1 className="p-5">Todo List</h1>
-				<div className="bg-info w-50 mx-auto" style={{height: "150px"}}>
-					<input className="form-control input input__lg p-3"  type="text" id="todo-input" value={inputValue} onChange={handleInputChange} name="text" autoComplete="off"/>
-         	 		<button className="btn btn-primary p-2" type="button" onClick={handleAddTask}>Agregar tarea</button>
+						<div className="form-group bg-info p-3">
+							<input
+							className="form-control mb-3"
+							type="text"
+							id="todo-input"
+							value={inputValue}
+							onChange={handleInputChange}
+							name="text"
+							autoComplete="off"
+							placeholder="Ingrese una tarea"
+							/>
+							<button
+							className="btn btn-outline-secondary"
+							type="button"
+							onClick={handleAddTask}
+							>
+							Agregar tarea
+							</button>
+						</div>
+					</div>
 				</div>
+			</div>
 
-				<div>
+			<div className="container">
+				<div className="row mx-auto" style={{width: "640px"}}>
 					<ul className="list-group">
 					{tarea.map((ele, index) => (
 					<li className="list-group-item" key={index}> {ele?.label} </li>
